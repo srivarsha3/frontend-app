af15a32e1ea4a74df1c803b63875239b17a05792
Nethi Sri Varsha <srivarshanethi@gmail.com>
2026-03-12 13:53:51 +0530

    updated

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,44 @@
-function Cart(){
-    return <h1>My Cart</h1>
+import { useContext } from "react";
+import { AppContext } from "../App";
+function Cart() {
+  const { cart, setCart } = useContext(AppContext);
+  const increment = (id) => {
+    setCart(
+      cart.map((item) => {
+        if (item._id === id) {
+          return { ...item, quantity: item.quantity + 1 };
+        } else {
+          return item;
+        }
+      }),
+    );
+  };
+  const decrement = (id) => {
+    setCart(
+      cart.map((item) => {
+        if (item._id === id && item.quantity > 0) {
+          return { ...item, quantity: item.quantity - 1 };
+        } else {
+          return item;
+        }
+      }),
+    );
+  };
+  return (
+    <div>
+      <h1>My Cart</h1>
+      <ol>
+        {cart.map((item) => (
+          <li key={item._id}>
+            {item.name}-{item.price}-
+            <button onClick={() => decrement(item._id)}>-</button>
+            {item.quantity}
+            <button onClick={() => increment(item._id)}>+</button>-
+            {item.quantity * item.price}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
-export default Cart
+export default Cart;
